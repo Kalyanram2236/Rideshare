857546738f3656d0e21b5501b94a8b0558fea2a3
sola.kalyan ram <160380740+Kalyanram2236@users.noreply.github.com>
2024-09-01 16:09:59 +0530

    App.py

---
+++ App.py
@@ -0,0 +1,18 @@
+%%writefile app.py
+!pip install streamlit
+import streamlit as st
+from user_dashboard import user_dashboard
+from driver_dashboard import driver_dashboard
+from admin_dashboard import admin_dashboard
+
+# Main navigation
+st.sidebar.title("Ride-Sharing App")
+option = st.sidebar.selectbox("Choose your dashboard", ("User", "Driver", "Admin"))
+
+# Routing to different dashboards
+if option == "User":
+    user_dashboard()
+elif option == "Driver":
+    driver_dashboard()
+elif option == "Admin":
+    admin_dashboard()
